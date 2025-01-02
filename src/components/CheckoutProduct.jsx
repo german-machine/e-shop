2929ed68cfa4fa14b6c1e88ddd57249b2c3d7201
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2025-01-02 02:12:27 +0100

    dynamically rendering products added to cart; function to remove product from cart/basket

--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
 import "./CheckoutProduct.css";
+import { useStateValue } from '../StateProvider/StateProvider';
 
-function CheckoutProduct() {
+function CheckoutProduct({id, title, image, price, rating }) {
+    const [{basket}, dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        dispatch({
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+        })
+    }
     return (
         <div className="checkoutProduct">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" className="checkoutProduct__image" />
+            <img src={image} alt="" className="checkoutProduct__image" />
 
             <div className="checkoutProduct__info">
-                <p className="checkoutProduct__title">Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellant Fabric for Men and Women (Blue)</p>
+                <p className="checkoutProduct__title">{title}</p>
                 <p className="checkoutProduct__price">
                     <small>$</small>
-                    <strong>20</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="checkoutProduct__rating">
-                    ⭐⭐
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐</p>
+                        ))
+                    }
                 </div>
-                <button>Remove from Basket</button>
+                <button onClick={removeFromBasket}>Remove from Basket</button>
             </div>
         </div>
     )
