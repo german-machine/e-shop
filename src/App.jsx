8ac7112162562a583273aa84e44f5fc52db21f12
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2025-01-02 02:00:59 +0100

    imported the App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Checkout from './components/Checkout';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import "./App.css"
 
 function App() {
   return (
